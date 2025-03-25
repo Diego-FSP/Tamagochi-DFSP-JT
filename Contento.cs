0061cfd4d179946e1b38c9406d65977e62c24e4e
Diego-FSP <diegomgplus014@hotmail.com>
2025-03-24 23:04:13 -0300

    fix 6
    fix

--- Contento.cs
+++ Contento.cs
@@ -2,10 +2,24 @@ using State;
 
 class Contento : State
 {
+    byte jugo=0;
     public override void Comer()
-    {}
-    public override void juegue()
-    {}
+    {
+        if(base.felicidad<10)
+        base.felicidad++;
+    }
+    public override void juegue(Tamagochi mascota)
+    {
+        if(jugo<5)
+        {
+            base.felicidad+=2;
+            jugo++;
+        }
+        else
+        mascota.CambiarState(new Hambrienta);
+    }
     public override void PuedeJugar()
-    {}
+    {
+
+    }
 }
